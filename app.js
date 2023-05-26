3c43bdb125ea953a3d2c7fc8441240900d1b9292
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-26 22:53:17 +0200

    JSON-data lavet. JSON-objekt hentet. Skal blot sættes ind i HTML med Dom-manipulation.

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-"use strict"
-console.log("Javascript kører!")
+"use strict";
 
+window.addEventListener("load", start);
+
+async function start() {
+  console.log("Javascript kører!");
+  const beers = await getData();
+  console.log(beers);
+  showBeers(beers);
+}
+
+async function getData() {
+  const response = await fetch("beer-cocktails.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showBeers(beers) {
+  let txt = "";
+  for (let x in beers) {
+    txt += beers[x] + " ";
+  }
+    console.log(txt);
+    
+}
